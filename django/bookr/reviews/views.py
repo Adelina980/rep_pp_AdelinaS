a7ec662b5d220074a0b8426a8242a2a8c2cf126e
Adelina <adelinasafarova84@gmail.com>
2023-10-25 21:53:13 +0300

    add

--- django/bookr/reviews/views.py
+++ django/bookr/reviews/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
-from .models import Book, Review
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book
 from .utils import average_rating
+
 
 def index(request):
     return render(request, "base.html")
+
+
 def book_search(request):
     search_text = request.GET.get("search", "")
     return render(request, "reviews/search-results.html", {"search_text": search_text})
 
+
 def book_list(request):
     books = Book.objects.all()
-    book_list = []
+    books_with_reviews = []
     for book in books:
         reviews = book.review_set.all()
         if reviews:
@@ -19,17 +24,12 @@ def book_list(request):
         else:
             book_rating = None
             number_of_reviews = 0
-        book_list.append({'book': book,
-                          'book_rating': book_rating,
-                          'number_of_reviews': number_of_reviews})
+        books_with_reviews.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
 
     context = {
-        'book_list': book_list
+        "book_list": books_with_reviews
     }
-    return render(request, 'bookr/reviews/templates/books_list.html', context)
+    return render(request, "reviews/book_list.html", context)
 
-
-
-
-
-
+def f(request):
+    return render(Book[request],'reviews/books/1')
